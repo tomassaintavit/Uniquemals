a0890fafe14371bbc3b9514c1ba0139737efe3c7
Tomas Saint Avit <tomas.saintavit@gmail.com>
2025-08-12 11:53:55 -0300

    [#4] Conexion con el back

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,6 @@
-import { useParams, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import countryTranslations from '../utils/country_translations.json';
@@ -9,90 +10,50 @@ const Country = () => {
   const spanishName = countryTranslations[name] || name;
   const location = useLocation();
   const countryData = location.state;
-  const [animals, setAnimals] = useState([]);
-  const countryName = decodeURIComponent(spanishName);
   
+  const [animales, setAnimales] = useState([]);
 
   useEffect(() => {
-    
-    const fetchEndemicAnimals = async () => {
-      try {
-        const categoryTitle = `Categoría:Fauna_endémica_de_${countryName.replace(/ /g, "_")}`;
-        const categoryUrl = `https://es.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=${encodeURIComponent(categoryTitle)}&cmtype=page&cmlimit=40&format=json&origin=*`;
-
-        const response = await fetch(categoryUrl);
-        const data = await response.json();
-        const pages = data.query?.categorymembers || [];
-
-        const animalDetails = await Promise.all(
-          pages.map(async (page) => {
-            const summaryUrl = `https://es.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(page.title)}`;
-            const res = await fetch(summaryUrl);
-            const summary = await res.json();
-            return {
-              title: summary.title,
-              extract: summary.extract,
-              image: summary.thumbnail?.source || null,
-            };
-          })
-        );
-
-        setAnimals(animalDetails);
-      } catch (error) {
-        console.error('Error al obtener los animales endémicos:', error);
-      }
-    };
-
-    fetchEndemicAnimals();
-  }, [countryName]);
+    fetch(`http://localhost:4000/animales/${decodeURIComponent(spanishName)}`)
+      .then(res => res.json())
+      .then(data => setAnimales(data))
+      .catch(err => console.error(err));
+  }, [name]);
 
   return (
     <div className="container mt-4">
-      <h2 className="mb-4">{countryName}</h2>
+      <h2 className="mb-4">{decodeURIComponent(spanishName)}</h2>
       <h4>Animales endémicos del país</h4>
       <div className="row">
-        <div className="col col-6">
+        <div className="col col-7">
           <table className="table">
             <thead className="thead-dark">
               <tr>
-                <th scope="col">Nombre</th>
-                <th scope="col">Características</th>
-                <th scope="col">Imagen</th>
+                <th>Nombre</th>
+                <th>Características</th>
+                <th>Imagen</th>
               </tr>
             </thead>
             <tbody>
-              {animals.length > 0 ? (
-                animals.map((animal, idx) => (
-                  <tr key={idx}>
-                    <td>{animal.title}</td>
-                    <td>{animal.extract}</td>
-                    <td>
-                      {animal.image ? (
-                        <img src={animal.image} alt={animal.title} width="100" />
-                      ) : (
-                        'Sin imagen'
-                      )}
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="3">Cargando datos o no se encontraron especies...</td>
+              {animales.map(a => (
+                <tr key={a.id}>
+                  <td>{a.name}</td>
+                  <td>{a.description}</td>
+                  <td>
+                    {a.image_url && <img src={a.image_url} alt={a.name} width="80" />}
+                  </td>
                 </tr>
-              )}
+              ))}
             </tbody>
           </table>
         </div>
-        <div className="col col-6">
-          <MapContainer
-            center={[countryData.lat, countryData.lng]}
-            zoom={4}
-            style={{ height: '50vh', width: '100%' }}
-          >
+
+        <div className="col col-5">
+          <MapContainer center={[countryData.lat, countryData.lng]} zoom={4} style={{ height: '50vh', width: '100%' }}>
             <TileLayer
               attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            /> 
           </MapContainer>
         </div>
       </div>
